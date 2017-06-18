e5a993caff3bf84d3670a6cdb90db98cff610f4c
acr <acarbey15@gmail.com>
2017-06-18 05:45:10 +0300

    yaz

--- src/routes.php
+++ src/routes.php
@@ -37,7 +37,7 @@ Route::group(['middleware' => ['web']], function () {
             Route::post('/card/payment', 'AcrSepetController@payment');
             Route::post('/card/payment/havale_eft', 'AcrSepetController@paymet_havale_eft');
             Route::get('/card/payment/havale_eft', 'AcrSepetController@paymet_havale_eft');
-            Route::post('/order/result', 'IyzicoController@order_result');
+            Route::post('/order/result', 'iyzicoController@order_result');
             Route::get('/card/payment/bank_card', 'AcrSepetController@payment_bank_card');
 
 
