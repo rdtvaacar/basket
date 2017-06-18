cbbd8d70b61548de69367c6fb0f322ebd627f4a6
acr <acarbey15@gmail.com>
2017-06-18 22:34:01 +0300

    yaz

--- src/Views/basarili_odeme.blade.php
+++ src/Views/basarili_odeme.blade.php
@@ -5,7 +5,7 @@
             <div class=" col-md-12">
                 <div class="box box-warning">
                     <div class="box-header with-border">BAŞARILI ÖDEME</div>
-                    <div class="box-body">
+                    <div class="box-body" style="text-align: center;">
                         <img src="/icon/suc.png"/>
                         <div class="alert alert-success" style="text-align: center; font-size: 16pt;">
                             Ödemeniz başarıyla gerçekleşti ve siparişleriniz tanımlandı. Teşekkür ederiz.
--- src/Views/basarisiz_odeme.blade.php
+++ src/Views/basarisiz_odeme.blade.php
@@ -5,7 +5,7 @@
             <div class=" col-md-12">
                 <div class="box box-warning">
                     <div class="box-header with-border">BAŞARISIZ ÖDEME</div>
-                    <div class="box-body">
+                    <div class="box-body" style="text-align: center">
                         <img src="/icon/hata.png"/>
                         <div class="alert alert-danger" style="text-align: center; font-size: 16pt;">
                             Ödemeniz gerçekleşmemiş lütfen tekrar deneyiniz. <a class="btn btn-warning" href="/acr/ftr/card/payment?order_id={{$order_id}}">Tekrar Dene</a> için tıklayınız.
