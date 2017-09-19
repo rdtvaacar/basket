615a7b0058fa1006a4701dda31282d9ba1c5c335
rdtvaacar <acarbey15@gmail.com>
2017-09-19 21:04:54 +0300

    yaz

--- src/Controllers/AcrSepetController.php
+++ src/Controllers/AcrSepetController.php
@@ -968,6 +968,7 @@ class AcrSepetController extends Controller
                 'archived'           => null,
                 'issue_date'         => date('Y-m-d'),
                 'details_attributes' => $parasut_product_data,
+                'total_paid'         => $sepet_row->price,
                 'payment_status'     => 'paid'
             ];
             $invoice = $parasut->sale($parasut_sale_data);
