0c4de14003f04a760c053e4f72840ec7e5f9c5c2
acr <acarbey15@gmail.com>
2017-12-08 00:49:29 +0300

    yaz

--- src/Model/Eticaret_user.php
+++ src/Model/Eticaret_user.php
@@ -9,4 +9,8 @@ class Eticaret_user extends Model
     protected $connection = 'mysql2';
     protected $table      = 'users';
 
+    function user()
+    {
+        return $this->hasOne('Acr\Ftr\Model\Eticaret_user', 'id', 'user_id');
+    }
 }
